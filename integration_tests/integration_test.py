fe989c63bc0a02e8b32b6c68e857df830b5607a0
Bruno Zuada Coelho <brunozuada@gmail.com>
2021-11-19 15:15:14 +0100

    added 2D integration tests
    Former-commit-id: 429f2b1b39c4d77f4b117dd415d63b92a322d747

--- integration_tests/integration_test.py
+++ integration_tests/integration_test.py
@@ -9,12 +9,14 @@ from src.scatter import scatter
 
 dec_places = 5
 
+
 def read_pickle(file):
     # read pickle file
     with open(file, "rb") as f:
         data = pickle.load(f)
 
     return data
+
 
 def assert_dict_almost_equal(expected, actual):
 
@@ -32,7 +34,8 @@ def assert_dict_almost_equal(expected, actual):
 
     return
 
-class Test1DWavePropagation(unittest.TestCase):
+
+class Test1DWavePropagation_3D(unittest.TestCase):
     def setUp(self):
 
         self.root = "integration_tests"
@@ -186,6 +189,117 @@ class Test1DWavePropagation(unittest.TestCase):
         data = read_pickle(os.path.join(self.fold_results, "data.pickle"))
 
         assert_dict_almost_equal(data, self.mean_data_high)
+        return
+
+    def tearDown(self):
+        shutil.rmtree(self.fold_results)
+        return
+
+
+class Test1DWavePropagation_2D(unittest.TestCase):
+    def setUp(self):
+
+        self.root = "integration_tests"
+        file = os.path.join(self.root, r"./results_mean_2d/data.pickle")
+        self.mean_data = read_pickle(file)
+        file = os.path.join(self.root, r"./results_rf_2d/data.pickle")
+        self.random_data = read_pickle(file)
+
+        self.fold_results = []
+        return
+
+    def test_1(self):
+        # computational settings
+        sett = {"gamma": 0.5,
+                "beta": 0.25,
+                "int_order": 2,
+                "damping": [1, 0.005, 20, 0.005],
+                "absorbing_BC": [1, 1],
+                "pickle": True,
+                "pickle_nodes": "all",
+                "VTK": False}
+
+        # boundary conditions
+        x = 1
+        y = 10
+        BC = {"bottom": ["01", [[0, 0, 0], [x, 0, 0]]],
+              "left": ["10", [[0, 0, 0], [0, y, 0]]],
+              "right": ["10", [[x, 0, 0], [x, y, 0]]],
+              }
+
+        # material dictionary: rho, E, v
+        mat = {"solid": {"density": 1500,
+                         "Young": 30e6,
+                         "poisson": 0.2},
+               "bottom": {"density": 1200,
+                          "Young": 300e6,
+                          "poisson": 0.25}}
+        load = {"force": [0, -1e6, 0],
+                "node": [3, 4, 25],
+                "time": 1.0,
+                "type": "heaviside"}
+
+        # run scatter
+        self.fold_results = os.path.join(self.root, "./_results_mean_2d")
+        scatter(os.path.join(self.root, r"./mesh/column_2D.msh"), self.fold_results, mat, BC, sett, load, time_step=5e-3)
+
+        # compare results
+        data = read_pickle(os.path.join(self.fold_results, "data.pickle"))
+
+        assert_dict_almost_equal(data, self.mean_data)
+        return
+
+    def test_2(self):
+        # computational settings
+        sett = {"gamma": 0.5,
+                "beta": 0.25,
+                "int_order": 2,
+                "damping": [1, 0.005, 20, 0.005],
+                "absorbing_BC": [1, 1],
+                "pickle": True,
+                "pickle_nodes": "all",
+                "VTK": False}
+
+        # boundary conditions
+        x = 1
+        y = 10
+        BC = {"bottom": ["01", [[0, 0, 0], [x, 0, 0]]],
+              "left": ["10", [[0, 0, 0], [0, y, 0]]],
+              "right": ["10", [[x, 0, 0], [x, y, 0]]],
+              }
+
+        # material dictionary: rho, E, v
+        mat = {"solid": {"density": 1500,
+                         "Young": 30e6,
+                         "poisson": 0.2},
+               "bottom": {"density": 1200,
+                          "Young": 300e6,
+                          "poisson": 0.25}}
+        load = {"force": [0, -1e6, 0],
+                "node": [3, 4, 25],
+                "time": 1.0,
+                "type": "heaviside"}
+
+        # Random field properties
+        RF_props = {"number_realisations": 1,
+                    "element_size": 1,
+                    "theta": 1,
+                    "seed_number": -26021981,
+                    "material": "solid",
+                    "key_material": "Young",
+                    "std_value": 3e6,
+                    "aniso_x": 2,
+                    "aniso_y": 5,
+                    }
+
+        # run scatter
+        self.fold_results = os.path.join(self.root, "./_results_rd_2d")
+        scatter(os.path.join(self.root, r"./mesh/column_2D.msh"), self.fold_results, mat, BC, sett, load, time_step=5e-3, random_props=RF_props)
+
+        # compare results
+        data = read_pickle(os.path.join(self.fold_results, "data.pickle"))
+
+        assert_dict_almost_equal(data, self.random_data)
         return
 
     def tearDown(self):
--- run_scatter_2D.py
+++ run_scatter_2D.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     sett = {"gamma": 0.5,
             "beta": 0.25,
             "int_order": 2,
-            "damping": [1, 0.05, 20, 0.05],
+            "damping": [1, 0.005, 20, 0.005],
             "absorbing_BC": [1, 1],
             "pickle": True,
             "pickle_nodes": "all",
@@ -17,22 +17,20 @@ if __name__ == "__main__":
     # boundary conditions
     x = 1
     y = 10
-    #z = -20
-    BC = {"bottom": ["01", [[0, 0,0], [x, 0, 0]]],
-          "left": ["10", [[0, 0,0], [0, y, 0]]],
-          "right": ["10", [[x, 0,0], [x, y, 0]]],
-
+    BC = {"bottom": ["01", [[0, 0, 0], [x, 0, 0]]],
+          "left": ["10", [[0, 0, 0], [0, y, 0]]],
+          "right": ["10", [[x, 0, 0], [x, y, 0]]],
           }
 
     # material dictionary: rho, E, v
-    mat = {"solid": {"density": 0.002000,
+    mat = {"solid": {"density": 1500,
                      "Young": 30e6,
                      "poisson": 0.2},
-           "bottom": {"density": 0.002500,
+           "bottom": {"density": 1200,
                       "Young": 300e6,
                       "poisson": 0.25}}
     load = {"force": [0, -1e6, 0],
-            "node": [3,4],
+            "node": [3, 4, 25],
             "time": 1.0,
             "type": "heaviside"}
 
@@ -49,12 +47,5 @@ if __name__ == "__main__":
                 }
 
     # run scatter
-    # scatter(r"./mesh/brick.msh", "./results_mn_mean", mat, BC, sett, load, time_step=5e-3, random_props=RF_props)
-    scatter(r"./mesh/column2.msh", "./results_tmp", mat, BC, sett, load, time_step=5e-3)
-    # scatter(r"./mesh/brick.msh", "./results_mn_mean", mat, BC, sett, load, time_step=5e-3)
-
-    # for i in range(1):
-    #     RF_props["seed_number"] = i
-    #     scatter(r"./mesh/brick.msh", "./results_mv_RF_1/run_" + str(i), mat, BC, sett, load,
-    #             time_step=1.5e-3, random_props=RF_props)
-#
+    scatter(r"./mesh/column_2D.msh", "./results_2D", mat, BC, sett, load, time_step=5e-3)
+    scatter(r"./mesh/column_2D.msh", "./results_2D_RF", mat, BC, sett, load, time_step=5e-3, random_props=RF_props)
