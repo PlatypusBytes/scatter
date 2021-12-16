3fd564a627fdf07c9e91d2a17c989a7c6eb8fed9
noordam <aron.noordam@deltares.nl>
2021-12-16 10:35:02 +0100

    committed scatter-rose mesh
    Former-commit-id: 89df33849af6a7ebb8cdb024d3cab65ad1c4a7ef

--- run_scatter_rose_2D.py
+++ run_scatter_rose_2D.py
@@ -57,13 +57,6 @@ if __name__ == "__main__":
 
 
     # run scatter
-    # scatter(r"./mesh/brick.msh", "./results_mn_mean", mat, BC, sett, load, time_step=5e-3, random_props=RF_props)
-    # scatter(r"./mesh/column2.msh", "./results_tmp", mat, BC, sett, load, time_step=5e-3)
-    scatter(r"./mesh/box2d.msh", "./results_tmp2", mat, BC, sett, load, time_step=1e-4)
-    # scatter(r"./mesh/brick.msh", "./results_mn_mean", mat, BC, sett, load, time_step=5e-3)
+    scatter(r"./mesh/box2d.msh", "./results_rose_2d", mat, BC, sett, load, time_step=1e-4)
 
-    # for i in range(1):
-    #     RF_props["seed_number"] = i
-    #     scatter(r"./mesh/brick.msh", "./results_mv_RF_1/run_" + str(i), mat, BC, sett, load,
-    #             time_step=1.5e-3, random_props=RF_props)
 #
