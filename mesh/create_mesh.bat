154743fbf9a4a1be5129398fd2a3c751432db899
Bruno Zuada Coelho <brunozuada@gmail.com>
2023-08-21 17:05:14 +0200

    added ex to create gmsh from geo file

--- mesh/create_mesh.bat
+++
@@ -1,3 +0,0 @@
-C:\Users\zuada\AppData\Local\gmsh-4.8.4-Windows64\gmsh.exe column.geo -format msh2 -3 -o column.msh
-C:\Users\zuada\AppData\Local\gmsh-4.8.4-Windows64\gmsh.exe column_2D.geo -format msh2 -2 -o column_2D.msh
-pause
