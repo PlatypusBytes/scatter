c06868bdb84141d639ffe80b89e4f7458c74f10f
brunozc <brunozuada@gmail.com>
2025-07-04 11:44:06 +0200

    Fix boundary condition and update material properties in run_scatter.py

--- run_scatter.py
+++ run_scatter.py
@@ -10,14 +10,14 @@ if __name__ == "__main__":
             "absorbing_BC": [1, 1],
             "absorbing_BC_stiff": 1e3,
             "pickle": True,
-            "pickle_nodes": "all",
+            "pickle_nodes": ["28"],
             "VTK": False,
             }
 
     x = 1
     y = 10
     z = -1
-    BC = {"bottom": ["020", [[0, 0, 0], [x, 0, 0], [0, 0, z], [x, 0, z]]],
+    BC = {"bottom": ["010", [[0, 0, 0], [x, 0, 0], [0, 0, z], [x, 0, z]]],
           "left": ["100", [[0, 0, 0], [0, 0, z], [0, y, 0], [0, y, z]]],
           "right": ["100", [[x, 0, 0], [x, 0, z], [x, y, 0], [x, y, z]]],
           "front": ["001", [[0, 0, 0], [z, 0, 0], [0, y, 0], [x, y, 0]]],
@@ -27,10 +27,8 @@ if __name__ == "__main__":
     # material dictionary: rho, E, v
     mat = {"solid": {"density": 1500,
                      "Young": 30e6,
-                     "poisson": 0.2},
-           "bottom": {"density": 1500.0,
-                      "Young": 30e6,
-                      "poisson": 0.2}}
+                     "poisson": 0.2}
+          }
 
     load = {"force": [0, -1000, 0],
             "node": [3, 4, 7, 8],
@@ -51,5 +49,8 @@ if __name__ == "__main__":
                 "aniso_z": 1,
                 }
 
+    from solvers import newmark_solver, central_difference_solver, bathe_solver
     # run scatter
-    scatter(r"./mesh/column.msh", "./results_abs", mat, BC, sett, load, time_step=0.05e-4, type_analysis="dynamic_explicit")
+    scatter(r"./mesh/column.msh", "./results", mat, BC, sett, load, time_step=0.01e-4, type_analysis="dynamic_explicit", solver=newmark_solver.NewmarkExplicit)
+#     scatter(r"./mesh/column.msh", "./results_lump_true_rowsum", mat, BC, sett, load, time_step=0.01e-4, type_analysis="dynamic_explicit", solver=central_difference_solver.CentralDifferenceSolver)
+#     scatter(r"./mesh/column.msh", "./results_bathe_lump_true_rowsum", mat, BC, sett, load, time_step=0.01e-4, type_analysis="dynamic_explicit", solver=bathe_solver.BatheSolver)
